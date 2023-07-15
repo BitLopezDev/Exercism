fb88be76cb6d4dde76597ce553139a715a72fca1
Santiago C <santiagocomesana03@gmail.com>
2023-07-15 17:05:07 -0300

    Bob

--- app/index.ts
+++ app/index.ts
@@ -34,3 +34,47 @@ export function toRna(dna: string = "ACGT") {
   }
   return rna;
 }
+
+
+export function hey(message: string ) :string {
+    var question :boolean = false;
+    var yell : boolean = false;
+    var silence : boolean =false;
+    var messagelast : string ='';
+   
+   if (message.trim().slice(-1) == '?') {
+    
+    question = true;
+    silence= false;
+   }
+   if (message.trim().length === 0  ) {
+    silence= true;
+    messagelast = 'Fine. Be that way!';
+   }
+
+   if(message === message.toUpperCase() && !silence && message.toUpperCase() !== message.toLocaleLowerCase()){
+    yell = true;
+    silence= false;
+
+   }
+
+   console.log(yell && question)
+  if (question && yell){
+    messagelast = 'Calm down, I know what I\'m doing!';
+  } else if (question){
+    messagelast = "Sure."
+  } else if (yell){
+    messagelast = "Whoa, chill out!";
+  } else if (silence) {
+    messagelast = "Fine. Be that way!"
+  }else {
+    messagelast = "Whatever."
+  }
+
+   return messagelast;
+  }
+  console.log(hey('\n\r \t'));
+
+
+
+  
